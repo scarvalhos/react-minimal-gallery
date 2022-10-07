418d582851c8eb7c4b32a9470558a30fee42286a
Samara Carvalho <samara.carvalho@quikdev.com.br>
2022-10-06 22:59:48 -0300

    [v] - 1.1.2

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+
+export default [
+  {
+    input: './src/index.tsx',
+    output: {
+        dir: 'dist',
+        format: 'cjs'
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react']
+      }),
+      external(),
+      resolve(),
+      terser(),
+      typescript(),
+      postcss({
+        config: {
+            path: './postcss.config.js'
+        },
+        use: {               
+            less: { javascriptEnabled: true }
+        },
+        extensions: ['.less'], // Looks like this still processes CSS despite this line.
+        extract: false         
+      }),
+      postcss({
+          config: {
+              path: './postcss.config.js'
+          },
+          extensions: ['.css'],
+          extract: false
+      })  
+    ]
+  }
+];
