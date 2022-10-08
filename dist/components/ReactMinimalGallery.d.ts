51420e1f43aea3587e752c52c0694848db36a2d9
Samara Carvalho <samara.carvalho@quikdev.com.br>
2022-10-07 23:57:05 -0300

    [v] - 1.1.5

---
+++ dist/components/ReactMinimalGallery.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { ReactMinimalGalleryProps } from '../@types/index';
+import '../styles.css';
+export declare const ReactMinimalGallery: React.FC<ReactMinimalGalleryProps>;
