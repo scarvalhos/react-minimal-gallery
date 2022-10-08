76364ebf3a4799f698d76141698367b7d0aacadc
Samara Carvalho <samara.carvalho@quikdev.com.br>
2022-10-08 00:25:58 -0300

    [1.0.1] - fix:types

--- dist/components/ReactMinimalGallery.stories.d.ts
+++ dist/components/ReactMinimalGallery.stories.d.ts
@@ -1,5 +1,5 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-export declare const ReactMinimalGalleryTemplate: ComponentStory<React.FC<import("../@types").ReactMinimalGalleryProps>>;
-declare const _default: ComponentMeta<React.FC<import("../@types").ReactMinimalGalleryProps>>;
+export declare const ReactMinimalGalleryTemplate: ComponentStory<React.FC<import("../type").ReactMinimalGalleryProps>>;
+declare const _default: ComponentMeta<React.FC<import("../type").ReactMinimalGalleryProps>>;
 export default _default;
