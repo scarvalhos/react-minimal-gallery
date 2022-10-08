a113da5a7e64958578e41ed4cb235fbb01453b08
Samara Carvalho <samara.carvalho@quikdev.com.br>
2022-10-08 15:53:48 -0300

    [1.1.2] - fix: exports

---
+++ dist/types/index.d.ts
@@ -0,0 +1,44 @@
+export interface FileRecord {
+  url: string
+  title: string
+}
+
+export type ReactMinimalGalleryProps = {
+  images: FileRecord[]
+  containerClassName?: string
+  mainImageClassName?: string
+  paginationColor?: string
+  hoverColor?: string
+  width?: string | number
+  thumbnailWidth?: string | number
+}
+
+declare namespace ReactMinimalGallery {
+  export const images: FileRecord[]
+
+  export const containerClassName: string | undefined
+
+  export const mainImageClassName: string | undefined
+
+  export const paginationColor: string | undefined
+
+  export const hoverColor: string | undefined
+
+  export const width: string | number | undefined
+
+  export const thumbnailWidth: string | number | undefined
+}
+
+declare module 'react-minimal-gallery' {
+  type ReactMinimalGallery = {
+    images: FileRecord[]
+    containerClassName?: string
+    mainImageClassName?: string
+    paginationColor?: string
+    hoverColor?: string
+    width?: string | number
+    thumbnailWidth?: string | number
+  }
+
+  export type ReactMinimalGalleryProps = ReactMinimalGallery
+}
