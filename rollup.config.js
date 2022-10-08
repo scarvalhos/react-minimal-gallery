1bf7db8b0aedf44dc9aad26284bf9fa8eedd2aee
Samara Carvalho <samara.carvalho@quikdev.com.br>
2022-10-08 01:20:22 -0300

    [1.0.7] - fix:types

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,15 @@ import external from 'rollup-plugin-peer-deps-external';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
+import sucrase from '@rollup/plugin-sucrase';
 
 export default [
   {
     input: './src/index.tsx',
     output: {
         dir: 'dist',
-        format: 'cjs'
+        format: 'cjs',
+        dir: "dist",
     },
     plugins: [
       babel({
@@ -20,7 +22,9 @@ export default [
       external(),
       resolve(),
       terser(),
-      typescript(),
+      typescript({
+        tsconfig: "tsconfig.json",
+      }),
       postcss({
         config: {
             path: './postcss.config.js'
@@ -38,7 +42,47 @@ export default [
         extensions: ['.less'], // Looks like this still processes CSS despite this line.
         extract: false         
       }),
-
+    ]
+  },
+  {
+    input: './src/types/index.d.ts',
+    output: {
+        dir: 'dist/types',
+        format: 'cjs',
+        dir: "dist",
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react']
+      }),
+      external(),
+      resolve(),
+      terser(),
+      sucrase({
+        exclude: ['node_modules/**'],
+        transforms: ['typescript', 'jsx'],
+      }),
+      typescript({
+        tsconfig: "tsconfig.json",
+      }),
+      postcss({
+        config: {
+            path: './postcss.config.js'
+        },
+        extensions: ['.css'],
+        extract: false
+      }),
+      postcss({
+        config: {
+            path: './postcss.config.js'
+        },
+        use: {               
+            less: { javascriptEnabled: true }
+        },
+        extensions: ['.less'], // Looks like this still processes CSS despite this line.
+        extract: false         
+      }),
     ]
   }
 ];
