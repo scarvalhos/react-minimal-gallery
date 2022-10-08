d3239b2b74bd7a3cf3fb2026d75f52ff5c979a51
Samara Carvalho <samara.carvalho@quikdev.com.br>
2022-10-08 00:35:23 -0300

    [1.0.1] - add dist

---
+++ dist/components/ReactMinimalGallery.stories.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+export declare const ReactMinimalGalleryTemplate: ComponentStory<React.FC<import("../@types").ReactMinimalGalleryProps>>;
+declare const _default: ComponentMeta<React.FC<import("../@types").ReactMinimalGalleryProps>>;
+export default _default;
